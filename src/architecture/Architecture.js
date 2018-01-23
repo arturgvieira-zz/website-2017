104a70577ff91e68b21fb97c9db24f5c84b0bb32
Artur G Vieira <vieira.artur.g@gmail.com>
2018-01-23 07:25:01 +0000

    Add new tweet

--- src/architecture/Architecture.js
+++ src/architecture/Architecture.js
@@ -9,6 +9,7 @@ class Architecture extends Component {
         <h2> I like Architecture, Software and Reading </h2>
         <blockquote className="twitter-tweet" data-lang="en"><p lang="en" dir="ltr">I just found a great book!<br/><br/>Clean Architecture: A Craftsman&#39;s Guide to Software Structure and Design <a href="https://t.co/oRufNlNa1u">https://t.co/oRufNlNa1u</a> … via <a href="https://twitter.com/safari?ref_src=twsrc%5Etfw">@safari</a></p>&mdash; Artur Vieira (@artur_g_vieira) <a href="https://twitter.com/artur_g_vieira/status/937843638964436992?ref_src=twsrc%5Etfw">December 5, 2017</a></blockquote>
         <blockquote class="twitter-tweet" data-lang="en"><p lang="en" dir="ltr">I put together a repo with the very essentials for a JavaEE JAX-RS API. <a href="https://t.co/LlYHtDIGz8">https://t.co/LlYHtDIGz8</a></p>&mdash; Artur G. Vieira (@artur_g_vieira) <a href="https://twitter.com/artur_g_vieira/status/948580808368902144?ref_src=twsrc%5Etfw">January 3, 2018</a></blockquote>
+        <blockquote class="twitter-tweet" data-lang="en"><p lang="en" dir="ltr">I just finished my first <a href="https://twitter.com/hashtag/GraphQL?src=hash&amp;ref_src=twsrc%5Etfw">#GraphQL</a> API, I really like how this stack works so I&#39;m sharing the project via GitHub repo.<a href="https://t.co/V4Th1OO5t1">https://t.co/V4Th1OO5t1</a></p>&mdash; Artur G. Vieira (@artur_g_vieira) <a href="https://twitter.com/artur_g_vieira/status/955700287976521729?ref_src=twsrc%5Etfw">January 23, 2018</a></blockquote>
       </div>
     );
   }
